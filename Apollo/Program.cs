94d3094950c56fea4851a66fdf6f99b25f99152c
altoweb <37968093+TheOnlyNoah11@users.noreply.github.com>
2020-06-18 08:20:35 +1200

    Init

---
+++ Apollo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apollo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string phrase = "Apollo"; // Strings
+            char grade = 'A'; // Chars (single letter)
+            int age = 14; // Integers (full numbers)
+            double gpa = 3.2; // Doubles (decimal numbers)
+            bool isMale = true; // Booleans (true/false)
+
+            Console.WriteLine(phrase);
+
+            Console.ReadLine();
+        }
+    }
+}
