e55c79a7a24c62ad92e6041b002f3e2d5d0f6cb4
altoweb <37968093+TheOnlyNoah11@users.noreply.github.com>
2020-06-28 11:46:51 +1200

    Move files around and add interactive

---
+++ Interactive/ApolloInteractive/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApolloInteractive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What's your name? : "); // Write text, do not skip a line so the user's input appears next to this text
+            string name = Console.ReadLine(); // Grabs user input and stores it in a variable
+            Console.Write("How old are you? : ");
+            string age = Console.ReadLine();
+            Console.WriteLine("Howdy, " + name + "! You are currently " + age + " years old."); // Print variables
+
+            Console.ReadLine();
+        }
+    }
+}
