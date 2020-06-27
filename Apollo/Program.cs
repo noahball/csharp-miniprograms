0d7287d9b69271f40b5445de1c16429eb62f59b3
altoweb <37968093+TheOnlyNoah11@users.noreply.github.com>
2020-06-28 09:40:20 +1200

    Finished working with strings

--- Apollo/Program.cs
+++ Apollo/Program.cs
@@ -16,7 +16,10 @@ namespace Apollo
             int age = 14; // Integers (full numbers)
             double gpa = 3.2; // Doubles (decimal numbers)
             bool isMale = true; // Booleans (true/false)
-            string lookFor = "Cool";
+            string lookFor = "Kids";
+            int firstInt = 6;
+            int printPart = 2;
+            int endPrintPart = 5;
 
             Console.WriteLine(phrase); // Write a line on the console - in this case a string (variable)
             Console.WriteLine("This is\nSkipping a line"); // Line breaks
@@ -27,8 +30,13 @@ namespace Apollo
             Console.WriteLine("The phrase " + phrase + " in lowercase is: " + phrase.ToLower()); // Converts a string into lowercase - using the ToLower() method
             Console.WriteLine("The myth that the phrase " + phrase + " contains " + lookFor + " is " + phrase.Contains(lookFor) + ", the mystery is solved!"); // Implementation of everything I've learned so far, and using the Contains() method while passing arguments
             Console.WriteLine("The first character of the string " + phrase + " is " + phrase[0]); // Print the first character of a string
+            Console.WriteLine("Character #" + firstInt + " of the string " + phrase + " is " + phrase[firstInt]); // Print a specific character from a string. In this case phrase is "Cool Kids" and firstInt is 6, the result will be i. Be aware that the first character is 0 NOT 1
+            Console.WriteLine("The phrase " + lookFor + " of the string " + phrase + " starts at the position #" + phrase.IndexOf(lookFor)); // Print the position where a specific word in a string starts.
+            Console.WriteLine("The string " + phrase + " trimmed to only display character #" + printPart + " and beyond is " + phrase.Substring(printPart)); // Only display a specific character of a string and beyond
+            Console.WriteLine("The characters in positions " + printPart + "-" + endPrintPart + " in the string " + phrase + " are " + phrase.Substring(printPart, endPrintPart)); // Display specific characters from a string in a specific range
 
-            Console.ReadLine(); // Show console lines until enter or a character is pressed
+            Console.WriteLine("Program executed successfully.");
+            Console.ReadLine(); // Show console lines until enter or a character is pressed. Without this the program will terminate immediately.
         }
     }
 }
