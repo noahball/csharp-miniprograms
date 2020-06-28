a239ba2265ef7e5adb4a3a2f42821bdc9f7aebe8
altoweb <37968093+TheOnlyNoah11@users.noreply.github.com>
2020-06-28 18:11:26 +1200

    Add Mad Libs game

---
+++ Mad Libs/ApolloMadLibs/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApolloMadLibs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string colour, pluralNoun, celebrity;
+
+            Console.Write("Please choose a colour: ");
+            colour = Console.ReadLine();
+
+            Console.Write("Please enter a plural noun: ");
+            pluralNoun = Console.ReadLine();
+
+            Console.Write("Please enter a celebrity's name: ");
+            celebrity = Console.ReadLine();
+
+            Console.WriteLine("Roses are " + colour);
+            Console.WriteLine(pluralNoun + " are blue");
+            Console.WriteLine("I Love " + celebrity);
+
+            Console.ReadLine();
+        }
+    }
+}
